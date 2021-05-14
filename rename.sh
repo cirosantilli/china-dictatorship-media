ff88b8f1f1d2ecf3597d759bdb4823fbd7300730
Ciro Santilli <ciro.santilli@gmail.com>
2021-05-14 08:00:07 +0000

    burn
    rename.sh
    
    more
    
    more
    
    bak

---
+++ rename.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -eux
+cp Chenmei_Caowei.png Chenmei_Caowei.jpg
+cp Falun_Gong_Ghuangzhou.jpg Falun_Gong_Guangzhou.jpg
+cp Jaign_zemin_magnify.jpg Jiang_Zemin_magnify.jpg
+cp Tianmen_crushed_dead.jpg Tiananmen_crushed_dead.jpg
+cp Tianmen_lesbian_kiss.jpg Tiananmen_lesbian_kiss.jpg
+cp Tianmen_sea.jpg Tiananmen_sea.jpg
