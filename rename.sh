713f69b71ffc7f0017708fff8eda2bdf22d7e667
Ciro Santilli <ciro.santilli@gmail.com>
2021-05-17 02:00:01 +0000

    fix

--- rename.sh
+++ rename.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -eux
-cp Chenmei_Caowei.png Chenmei_Caowei.jpg
-cp Falun_Gong_Ghuangzhou.jpg Falun_Gong_Guangzhou.jpg
-cp Jaign_zemin_magnify.jpg Jiang_Zemin_magnify.jpg
-cp Tianmen_crushed_dead.jpg Tiananmen_crushed_dead.jpg
-cp Tianmen_lesbian_kiss.jpg Tiananmen_lesbian_kiss.jpg
-cp Tianmen_sea.jpg Tiananmen_sea.jpg
+convert Chenmei_Caowei.jpg Chenmei_Caowei.png
+cp Falun_Gong_Guangzhou.jpg Falun_Gong_Ghuangzhou.jpg
+cp Jiang_Zemin_magnify.jpg Jaign_zemin_magnify.jpg
+cp Tiananmen_crushed_dead.jpg Tianmen_crushed_dead.jpg
+cp Tiananmen_lesbian_kiss.jpg Tianmen_lesbian_kiss.jpg
+cp Tiananmen_sea.jpg Tianmen_sea.jpg
