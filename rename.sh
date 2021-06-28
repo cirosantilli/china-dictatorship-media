cd3eadc18f703bd3a258a5e26e71b6d4d6599792
Ciro Santilli <ciro.santilli@gmail.com>
2021-06-28 04:00:03 +0000

    chenmei

--- rename.sh
+++ rename.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -eux
-convert Chenmei_Caowei.jpg Chenmei_Caowei.png
+convert Chenmei_Caowei.png Chenmei_Caowei.jpg
 cp Falun_Gong_Guangzhou.jpg Falun_Gong_Ghuangzhou.jpg
 cp Jiang_Zemin_magnify.jpg Jaign_zemin_magnify.jpg
 cp Tiananmen_crushed_dead.jpg Tianmen_crushed_dead.jpg
